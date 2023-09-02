535b0e41769394dc0470c6f7a71d67f0fb6296e6
gunnar-miklis <gunnar.miklis@gmail.com>
2023-09-02 15:58:51 +0200

    readability

--- jest-intro/examples/calculator.test.js
+++ jest-intro/examples/calculator.test.js
@@ -1,30 +1,37 @@
 const calculator = require( './calculator' );
 
-// "Test Suite", like a collection of tests
-describe( 'simple calculator example', () => {
-	
-	// description of what being test, and how the code behaves.
-	it('adds 25 + 5 to equal 30', () => {
-		// run/execute a sample code/data
-		const resultOfAdd = calculator.add( 25, 5 );
+// Readable "test-flow"
+//	* describe "feature"...
+//	* it should do XYZ...
+//	* expect XYZ to [be/equal/etc] XYZ.
 
-		// run some tests
-		expect( typeof resultOfAdd ).toBe( 'number' );
-		expect( resultOfAdd ).toBe( 30 );
-	})
-	it('substract 25 - 5 to equal 20', () => {
-		const resultOfSub = calculator.sub( 25, 5 );
-		expect( typeof resultOfSub ).toBe( 'number' );
-		expect( resultOfSub ).toBe( 20 );
-	})
-	it('mulitplies 25 * 5 to equal 125', () => {
-		const resultOfMul = calculator.mul( 25, 5 );
-		expect( typeof resultOfMul ).toBe( 'number' );
-		expect( resultOfMul ).toBe( 125 );
-	})
-	it('divides 25 / 5 to equal 5', () => {
-		const resultOfDiv = calculator.div( 25, 5 );
-		expect( typeof resultOfDiv ).toBe( 'number' );
-		expect( resultOfDiv ).toBe( 5 );
-	})
-})
+// "Test Suite", like a collection of tests
+describe( 'calculator', () => {
+	// description of what being test, and how the code behaves.
+	it( 'should be a type number and should add 25 + 5 to equal 30', () => {
+		// run/execute a sample code/data, etc.
+		const result = calculator.add( 25, 5 );
+
+		// run a tests
+		expect( typeof result ).toBe( 'number' );
+		expect( result ).toBe( 30 );
+	} );
+
+	it( 'should be a number and substract 25 - 5 to equal 20', () => {
+		const result = calculator.sub( 25, 5 );
+		expect( typeof result ).toBe( 'number' );
+		expect( result ).toBe( 20 );
+	} );
+
+	it( 'should be a number and mulitply 25 * 5 to equal 125', () => {
+		const result = calculator.mul( 25, 5 );
+		expect( typeof result ).toBe( 'number' );
+		expect( result ).toBe( 125 );
+	} );
+
+	it( 'should be a number and divide 25 / 5 to equal 5', () => {
+		const result = calculator.div( 25, 5 );
+		expect( typeof result ).toBe( 'number' );
+		expect( result ).toBe( 5 );
+	} );
+} );
