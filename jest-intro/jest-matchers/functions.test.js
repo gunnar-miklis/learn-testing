2b2a379707632d20ea7e5eb741a8916c81fc7e8c
gunnar-miklis <gunnar.miklis@gmail.com>
2023-09-02 15:59:48 +0200

    add example for mock log

--- jest-intro/jest-matchers/functions.test.js
+++ jest-intro/jest-matchers/functions.test.js
@@ -1,15 +1,26 @@
-function myFn(num, str) {
+function myFn( num, str ) {
 	return 'Done!';
 }
 
 test( 'use different matchers to test functions', () => {
-	
 	// mock function to call it "indirectly"
-	const mockFn = jest.fn( myFn )
-	mockFn( 123, 'word');
+	const mockFn = jest.fn( myFn );
+	mockFn( 123, 'word' );
+
+	console.log( mockFn.mock );
+	// log
+	// {
+	//   calls: [ [ 123, 'word' ] ],
+	//   contexts: [ undefined ],
+	//   instances: [ undefined ],
+	//   invocationCallOrder: [ 1 ],
+	//   results: [ { type: 'return', value: 'Done!' } ],
+	//   lastCall: [ 123, 'word' ]
+	// }
+	console.log( mockFn.mock.results[0].value ); // :>> "Done!"
 
 	expect( mockFn ).toHaveBeenCalled();
 	expect( mockFn ).toHaveBeenCalledWith( 123, 'word' );
 	expect( mockFn ).toHaveReturned();
 	expect( mockFn ).toHaveReturnedWith( 'Done!' );
-})
+} );
