c19a27286bb84e175730d8a30b23b47f9c6ac160
gunnar-miklis <gunnar.miklis@gmail.com>
2023-09-02 15:59:01 +0200

    comment

--- jest-intro/examples/subNums.js
+++ jest-intro/examples/subNums.js
@@ -1,10 +1,11 @@
 function subNums( num1, num2 ) {
-	return num1 - num2
+	return num1 - num2;
 }
 
+// Show an example on how JEST deals with stuff like this...
 function untestedCode() {
 	console.log(
-		'This is untested Code. JEST will notify about this: "6 Uncovered Lines. 66% coverage.'
+		'This is untested Code. JEST will notify about this: "6 Uncovered Lines. 66% coverage.',
 	);
 }
 
